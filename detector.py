8a64c91b996ba0949bd965977e3d681be9af80b6
orujahmadov <oruc888@yahoo.com>
2017-10-21 23:56:43 -0400

    data 10 -100

--- detector.py
+++ detector.py
@@ -123,11 +123,9 @@ def preprocess_data():
 
 def build_classifier():
     classifier = Sequential()
-    classifier.add(Dense(units = 37, kernel_initializer = 'uniform', activation = 'relu', input_dim = 69))
-    classifier.add(Dense(units = 37, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 37, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 37, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 37, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 49, kernel_initializer = 'uniform', activation = 'relu', input_dim = 69))
+    classifier.add(Dense(units = 49, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 49, kernel_initializer = 'uniform', activation = 'relu'))
     classifier.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
