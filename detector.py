5a12d66b42ee5c2a266d60010e58961a1ed6a45a
orujahmadov <oruc888@yahoo.com>
2017-10-22 01:11:37 -0400

    neural net 6

--- detector.py
+++ detector.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     # Save kaggle test results to submit to competition        
     export_kaggle_results('kaggle/neuralNetsAdamCategorical49.csv', 'Id','Category', y_test_results)
     
-    pickle.dump(classifier, open('models/neuralNetsAdamCategorical49.sav', 'w'), protocol=2)
+    pickle.dump(classifier, open('models/neuralNetsAdamCategorical49.sav', 'wb'), protocol=2)
     
 
     
